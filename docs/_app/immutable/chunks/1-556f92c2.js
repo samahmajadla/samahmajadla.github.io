5dc5b7c41114979e99c1e97e3865e78a505eced6
Samah Majadla <smajadla@smajadlaY16RJ.vmware.com>
2022-12-15 15:05:03 -0500

    got rid of url

---
+++ docs/_app/immutable/chunks/1-556f92c2.js
@@ -0,0 +1,1 @@
+import{default as t}from"../components/error.svelte-10f4c5c4.js";export{t as component};
--- docs/_app/immutable/chunks/1-c5af158e.js
+++
@@ -1,1 +0,0 @@
-import{default as t}from"../components/error.svelte-0e3bedfa.js";export{t as component};
