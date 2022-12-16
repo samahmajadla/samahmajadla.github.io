c20775e4696f5a164453d2be6c3b8dd23d18f2d6
Samah Majadla <smajadla@smajadlaY16RJ.vmware.com>
2022-12-16 14:18:11 -0500

    changed wording around application notifications

---
+++ docs/_app/immutable/chunks/1-80c96203.js
@@ -0,0 +1,1 @@
+import{default as t}from"../components/error.svelte-0c224508.js";export{t as component};
--- docs/_app/immutable/chunks/5-9c47907e.js
+++
@@ -1,1 +0,0 @@
-import{default as t}from"../components/pages/mentorship/_page.svelte-f4e599bb.js";export{t as component};
